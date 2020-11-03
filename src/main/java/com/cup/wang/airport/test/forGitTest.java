7902f773e7c0087aa9ab8ea161839e8b026bf837
WangHH51322 <1252011170@qq.com>
2020-11-03 10:28:58 +0800

    2020-11-03 用来测试远程仓库

---
+++ src/main/java/com/cup/wang/airport/test/forGitTest.java
@@ -0,0 +1,12 @@
+package com.cup.wang.airport.test;
+
+/**
+ * @author Qing
+ * @version 1.0
+ * @date 2020/11/3 10:27
+ */
+public class forGitTest {
+    public static void main(String[] args) {
+        System.out.println("用来测试远程仓库");
+    }
+}
